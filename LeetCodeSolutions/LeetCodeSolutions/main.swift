8e1b986a1928b8506bd75b1aa1b55eb36f769ac6
杨维 <swkj9318@qq.com>
2017-02-23 18:17:18 +0800

    创建工程

---
+++ LeetCodeSolutions/LeetCodeSolutions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCodeSolutions
+//
+//  Created by bjyangwei on 17/2/23.
+//  Copyright © 2017年 bjyangwei. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
