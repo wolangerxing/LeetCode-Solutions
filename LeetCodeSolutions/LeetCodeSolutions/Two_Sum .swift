bd9ed31124881414c64b009d453807309532e79a
杨维 <swkj9318@qq.com>
2017-02-24 15:53:27 +0800

    修改注释

--- LeetCodeSolutions/LeetCodeSolutions/Two_Sum .swift
+++ LeetCodeSolutions/LeetCodeSolutions/Two_Sum .swift
@@ -6,19 +6,25 @@
 //  Copyright © 2017年 bjyangwei. All rights reserved.
 //
 
-//https://oj.leetcode.com/problems/two-sum/
-//
-//#1 Two Sum
-//
-//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-//
-//You may assume that each input would have exactly one solution, and you may not use the same element twice.
-//
-//Example:
-//Given nums = [2, 7, 11, 15], target = 9,
-//
-//Because nums[0] + nums[1] = 2 + 7 = 9,
-//return [0, 1].
+/**
+ https://oj.leetcode.com/problems/two-sum/
+ 
+ #1 Two Sum
+ 
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ 
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ 
+ Example:
+ Given nums = [2, 7, 11, 15], target = 9,
+ 
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ */
+
+/**
+ 思路：遍历一遍，利用hashMap
+ */
 
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int]  {
