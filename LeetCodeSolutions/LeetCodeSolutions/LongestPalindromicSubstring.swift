f81818c43b8ade347ede9174f6216eb86ce3e95c
杨维 <swkj9318@qq.com>
2017-03-03 17:31:44 +0800

    第五个问题

---
+++ LeetCodeSolutions/LeetCodeSolutions/LongestPalindromicSubstring.swift
@@ -0,0 +1,36 @@
+//
+//  LongestPalindromicSubstring.swift
+//  LeetCodeSolutions
+//
+//  Created by bjyangwei on 17/3/3.
+//  Copyright © 2017年 bjyangwei. All rights reserved.
+//
+
+/**
+https://leetcode.com/problems/longest-palindromic-substring/
+ 
+ #5 Longest Palindromic Substring
+ 
+ Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ 
+ Example:
+ 
+ Input: "babad"
+ 
+ Output: "bab"
+ 
+ Note: "aba" is also a valid answer.
+ Example:
+ 
+ Input: "cbbd"
+ 
+ Output: "bb"
+ */
+
+/**
+ 思路：
+ */
+
+class LongestPalidromicSubstringSolution{
+    
+}
--- LeetCodeSolutions/LeetCodeSolutions/MedianOfTwoSortedArrays.swift
+++ LeetCodeSolutions/LeetCodeSolutions/MedianOfTwoSortedArrays.swift
@@ -9,7 +9,7 @@
 /**
  https://leetcode.com/problems/median-of-two-sorted-arrays/
 
- #3 Median of Two Sorted Arrays
+ #4 Median of Two Sorted Arrays
  
  There are two sorted arrays nums1 and nums2 of size m and n respectively.
  
